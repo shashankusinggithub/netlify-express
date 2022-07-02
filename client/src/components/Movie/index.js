3ab6c51cacd48fe75b3e63815a5910ed51d2293c
shashankusinggithub <shashank.chutke@gmail.com>
2022-07-03 04:45:16 +0530

    pre deployement fix

--- client/src/components/Movie/index.js
+++ client/src/components/Movie/index.js
@@ -25,7 +25,7 @@ const Movie = () => {
     }
 
     const getMovieRequest = async (searchValue) => {
-        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+        const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
         const response = await fetch(url);
         const responseJson = await response.json();
