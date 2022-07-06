ffc371fc0a4e6735712c90ca70bff1200ffba36b
shashankusinggithub <shashank.chutke@gmail.com>
2022-07-06 18:53:43 +0530

    final eith playlist

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ app.use("/selfplaylist", selfPlaylist )
 
 
 
-// if (process.env.NODE_ENV == 'production'){
-//     app.use(express.static('client/build'));
-//     app.get('*', (req,res)=> {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
-//     });
-// }
+if (process.env.NODE_ENV == 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000
 
